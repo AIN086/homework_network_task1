7b181a4a862327c2390eda5ac39659e465ec8bce
AIN086 <novozhilov952@gmail.com>
2022-05-28 19:27:17 +0500

    Added correction

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -14,16 +14,20 @@ public class Server {
 
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             while (true) {
-                Socket clientSocket = serverSocket.accept(); // ждем подключения
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                try (Socket clientSocket = serverSocket.accept();
+                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
 
-                System.out.println("New connection accepted");
+                    System.out.println("New connection accepted");
 
-                final String name = in.readLine();
-                out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+                    final String name = in.readLine();
+                    out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
