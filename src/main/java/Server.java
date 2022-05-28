8a49eb6cc8d4e6d505fbb1c7f8e5033660d7fbd4
AIN086 <novozhilov952@gmail.com>
2022-05-28 19:13:10 +0500

    Added try-source in Server

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -12,18 +12,18 @@ public class Server {
         System.out.println("Server started");
         int port = 8089;
 
-        ServerSocket serverSocket = new ServerSocket(port);
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            while (true) {
+                Socket clientSocket = serverSocket.accept(); // ждем подключения
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-        while (true){
-            Socket clientSocket = serverSocket.accept(); // ждем подключения
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                System.out.println("New connection accepted");
 
-            System.out.println("New connection accepted");
+                final String name = in.readLine();
+                out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+            }
 
-            final String name = in.readLine();
-            out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
         }
-
     }
 }
